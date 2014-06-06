5aac2c71638690781bdecf8e08f722870727a6db
Graham Cox <graham@grahamcox.co.uk>
2014-06-06 19:54:57 +0100

    Added JLine for an interactive console

--- server/app/src/main/java/uk/co/grahamcox/cpmmo/MainServerApp.java
+++ server/app/src/main/java/uk/co/grahamcox/cpmmo/MainServerApp.java
@@ -10,12 +10,19 @@
  ******************************************************************************************************************/
 package uk.co.grahamcox.cpmmo;
 
+import jline.console.ConsoleReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.grahamcox.cpmmo.network.Server;
+
+import java.io.IOException;
 
 /**
  * The main app for the server
  */
 public class MainServerApp implements MainApp {
+    /** The logger to use */
+    private static final Logger LOG = LoggerFactory.getLogger(MainServerApp.class);
     /** The actual network server */
     private Server server;
 
@@ -33,6 +40,34 @@ public class MainServerApp implements MainApp {
     @Override
     public void run() {
         server.start();
+        runConsole();
         server.stop();
+    }
+
+    /**
+     * Run the console
+     */
+    private void runConsole() {
+        try {
+            ConsoleReader consoleReader = new ConsoleReader();
+            runInteractiveConsole(consoleReader);
+        } catch (IOException e) {
+            LOG.warn("Failed to open JLine. Console commands not available", e);
+        }
+    }
+
+    /**
+     * Run the interactive console
+     * @param consoleReader the console reader to use
+     */
+    private void runInteractiveConsole(ConsoleReader consoleReader) {
+        boolean running = true;
+        while (running) {
+            try {
+                String line = consoleReader.readLine("> ");
+            } catch (IOException e) {
+                LOG.warn("Error reading from console", e);
+            }
+        }
     }
 }
