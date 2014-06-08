49cbcf1039498e414707c158ac608e7b217db8c9
Graham Cox <graham@grahamcox.co.uk>
2014-06-08 10:39:42 +0100

    Encoder that does the opposite of the chunked decoder, writing the byte length and then the bytes

---
+++ server/network/src/main/java/uk/co/grahamcox/cpmmo/network/ChunkedMessageEncoder.java
@@ -0,0 +1,22 @@
+package uk.co.grahamcox.cpmmo.network;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Channel Handler that will encode messages into chunks, using a leading length to determine chunk size
+ */
+public class ChunkedMessageEncoder extends MessageToByteEncoder<byte[]> {
+    /**
+     * Encode the given byte array by sending a 4-byte length then the actual bytes
+     * @param ctx the context
+     * @param msg the bytes to send
+     * @param out the buffer to write to
+     */
+    @Override
+    protected void encode(ChannelHandlerContext ctx, byte[] msg, ByteBuf out) {
+        out.writeInt(msg.length);
+        out.writeBytes(msg);
+    }
+}
